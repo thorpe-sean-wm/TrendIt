ab941f91b2b293612341a069a567007a803ca812
Jacob Burton <session1@START-Coding-MBP-03.local>
2016-04-27 15:13:36 -0700

    Setup Template with working PHP

---
+++ footer.php
@@ -0,0 +1,5 @@
+<div class="footerCont">
+    <div class="footer">
+        <h4>Copyright &copy; 2016 Kiwi Corp</h4>
+    </div>
+</div>
--- index.php
+++ index.php
@@ -1,2 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+
+    <meta charset="UTF-8">
+    <title>Trend It</title>
+    <link href="mainstylesheet.css" rel="stylesheet" type="text/css">
+    <link href='https://fonts.googleapis.com/css?family=Muli' rel='stylesheet' type='text/css'>
+
+</head>
+
+<body>
 <?php
-ihihii
+require_once "header.php";
+?>
+<div class="container">
+    <div class="content">
+
+    </div>
+</div>
+<?php
+require_once "footer.php";
+?>
+</body>
+
+</html>
