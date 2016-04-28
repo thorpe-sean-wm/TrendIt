d132ee3ebc9f04bab12275f129c41ca71355b96b
Sean Thorpe <sean.thorpe@west-mec.org>
2016-04-28 15:15:36 -0700

    Added an additional button on nav for the profile page

--- header.php
+++ header.php
@@ -2,6 +2,7 @@
     <div class="header">
         <nav class="navbar">
             <a href="index.php"><button class="mButton">Home</button></a>
+            <a href="index.php"><button class="mButton">Profile</button></a>
             <a href="search.php"><button class="mButton">Search</button></a>
             <a href="login.php"><button class="mButton">Log In</button></a>
             <a href="signup.php"><button class="mButton">Sign Up</button></a>
