c6b13bb1554956007d830bcad07a4846156f0692
Jacob Burton <session1@START-Coding-MBP-03.local>
2016-05-02 14:58:38 -0700

    Change to Header to be find logout button.

--- header.php
+++ header.php
@@ -1,11 +1,21 @@
+<?php
+session_start();
+?>
 <div class="headerCont">
     <div class="header">
         <nav class="navbar">
             <a href="index.php"><button class="mButton">Home</button></a>
             <a href="index.php"><button class="mButton">Profile</button></a>
             <a href="search.php"><button class="mButton">Search</button></a>
-            <a href="login.php"><button class="mButton">Log In</button></a>
-            <a href="signup.php"><button class="mButton">Sign Up</button></a>
+            <?php
+            if(isset($_SESSION['user_id'])){
+                echo '<a href="logout.php"><button class="mButton">Log Out</button></a>';
+            }
+            else{
+                echo '<a href="login.php"><button class="mButton">Log In</button></a>';
+                echo '<a href="signup.php"><button class="mButton">Sign Up</button></a>';
+            }
+            ?>
         </nav>
         <img id="logo" src="images/trendit.png" height="100px" alt="Trend It">
     </div>
