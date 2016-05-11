bd903f1cef34842fb010cb2cb1f43107fe30b78a
Jacob Burton <session1@START-Coding-MBP-03.local>
2016-05-11 15:12:32 -0700

    Fixed CSS issues

--- viewprofile.php
+++ viewprofile.php
@@ -29,7 +29,7 @@ require_once "header.php";
         <div id="profileLeftBar">
             <div id="userphoto" align="center"><img src="images/avatar.png" alt="default avatar" width="173" ></div>
             <div class="friend" align="left">
-                <p>Friends list:</p>
+                <h4>Friends list:</h4>
                 <?php
                 // Retrieve the score data from MySQL
                 $query = "SELECT * FROM followers WHERE userID = '" . $pageInfo['userID'] . "'";
@@ -51,7 +51,7 @@ require_once "header.php";
                     );
                     $userinfo = $stmt->fetch();
 
-                    echo '<li><a href="?user=' . $userinfo['userID'] . '"><img src="images/avatar.png" width="22" height="22">'. $userinfo['username'] .'</a></li>';
+                    echo '<li><a href="?user=' . $userinfo['userID'] . '"><img src="images/avatar.png" width="22" height="22">&nbsp;'. $userinfo['username'] .'</a></li>';
                 }
 
                 echo '</ul>';
@@ -61,8 +61,11 @@ require_once "header.php";
         <div id="userBio">
             <h1 id="viewprofileTitle"><?php echo $pageInfo['username']; ?></h1>
 
-
-            <a href="editprofile.php" class="gear"><img src="https://cdn4.iconfinder.com/data/icons/ionicons/512/icon-ios7-gear-128.png" height="25" width="25"></a>
+            <?php
+            if($_SESSION['userID'] == $pageInfo['userID']){
+                echo '<a href="editprofile.php" class="gear"><img src="https://cdn4.iconfinder.com/data/icons/ionicons/512/icon-ios7-gear-128.png" height="25" width="25"></a>';
+            }
+            ?>
         </div>
 
         <div class="feed">
