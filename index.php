d1a70bc5c7835aec82a30420180e899e7ee2a979
Azaniyah Shephard <session1@START-Coding-MBP-22.local>
2016-05-11 15:17:11 -0700

    Changed up the welcome page

--- index.php
+++ index.php
@@ -65,5 +65,7 @@ require_once "header.php";
 <?php
 require_once "footer.php";
 ?>
+
+<a href="welcomeIndex.php">Hello</a>
 </body>
 </html>
---
+++ welcomeIndex.php
@@ -0,0 +1,56 @@
+<html>
+<link href="http://fonts.googleapis.com/css?family=Source+Sans+Pro:300,600" rel="stylesheet" type="text/css">
+<!--[if lte IE 8]>
+<script src="js/html5shiv.js">
+<![endif]-->
+<script src="js/jquery.min.js"></script>
+<script src="js/jquery.dropotron.js"></script>
+<script src="js/skel.min.js"></script>
+<script src="js/skel-panels.min.js"></script>
+<script src="js/init.js"></script>
+<noscript>
+    <link rel="stylesheet" href="css/skel-noscript.css">
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/style-n1.css">
+</noscript>
+</head>
+<body class="homepage">
+<div id="header-wrapper">
+    <div id="header" class="container">
+        <h1 id="logo"><a href="#">Trendit</a></h1>
+
+    </div>
+    <section id="hero" class="container">
+        <header>
+            <!--TITLE OF PAGE -->
+            <h2>Welcome</h2>
+        </header>
+        <p>Your now favorite social media website</p>
+        <ul class="actions">
+
+            <li><a href="index.php" class="button">#DatBoy</a></li>
+
+        </ul>
+        <div class="log">
+            <a href="login.php" class="button">Login</a>
+
+           <a href="signup.php" class="button">Sign Up</a>
+
+        </div>
+    </section>
+
+
+</div>
+
+
+</html>
+
+
+
+<?php
+/**
+ * Created by PhpStorm.
+ * User: session1
+ * Date: 5/10/16
+ * Time: 2:04 PM
+ */
