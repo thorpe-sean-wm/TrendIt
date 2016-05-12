97ba965e4639a9ea6edd4b3422efd9b8c7130cf5
Start Coding Student 1 <session1@START-Coding-MBP-07.local>
2016-05-12 13:55:00 -0700

    Made the search bar even better :) ;) :D :P

--- search.php
+++ search.php
@@ -40,191 +40,137 @@ require_once "header.php";
         <div id="searchResults">
             <?php
             if (isset($_GET['submit']) && !($_GET['searchBar'] == '')) {
-                if (!empty($_GET['searchBar']) && !empty($_GET['searchBar'])) {
-                    if ((!isset($_GET['userNameSearch'])) && (!isset($_GET['firstNameSearch'])) && (!isset($_GET['lastNameSearch']))) {
-                        echo '<p class=error>Please check at least one search category.</p>';
-                    }
-                    // The search form is valid, we start the search and connect to the database
-                    else {
-                        // Connect to the database
-                        $dbh = new PDO('mysql:host=localhost;dbname=trenditdb', 'root', 'root');
+//                if (!empty($_GET['searchBar']) && !($_GET['searchBar'] == '')) {
+                if ((!isset($_GET['userNameSearch'])) && (!isset($_GET['firstNameSearch'])) && (!isset($_GET['lastNameSearch']))) {
+                    echo '<p class=error>Please check at least one search category.</p>';
+                }
+                // The search form is valid, we start the search and connect to the database
+                else {
+                    // Connect to the database
+                    $dbh = new PDO('mysql:host=localhost;dbname=trenditdb', 'root', 'root');
 
-                        // Setting what the value is inside the search bar
-                        $searchInput = $_GET['searchBar'];
-                        // Set default search options (shouldn't ever be used if everything works as planned)
-                        $optUserName = false;
-                        $optFirstName = false;
-                        $optLastName = false;
-                        // fetch the search options for the query
-                        if(isset($_GET['userNameSearch'])){$optUserName = true;}else{$optUserName = false;}
-                        if(isset($_GET['firstNameSearch'])){$optFirstName = true;}else{$optFirstName = false;}
-                        if(isset($_GET['lastNameSearch'])){$optLastName = true;}else{$optLastName = false;}
-                        // Writing the correct query to fetch the correct data from the database
-                            if ($optUserName == 1) {
-                            if ($optFirstName == 1) {
-                                if ($optLastName == 1) {
-                                    $query = "SELECT username, firstname, lastname FROM users WHERE username = :searchInput OR firstName = :firstInput OR lastName = :lastInput";
-                                    $stmt = $dbh->prepare($query);
-                                    $stmt->execute(array(
-                                        'searchInput' => $searchInput,
-                                        'firstInput' => $searchInput,
-                                        'lastInput' => $searchInput
-                                    ));
-                                }
-                                else{
-                                    $query = "SELECT username, firstname, lastname FROM users WHERE username = :searchInput OR firstName = :firstInput";
-                                    $stmt = $dbh->prepare($query);
-                                    $stmt->execute(array(
-                                        'searchInput' => $searchInput,
-                                        'firstInput' => $searchInput
-                                    ));
-                                }
+                    // Setting what the value is inside the search bar
+                    $searchInput = $_GET['searchBar'];
+                    // Set default search options (shouldn't ever be used if everything works as planned)
+                    $optUserName = false;
+                    $optFirstName = false;
+                    $optLastName = false;
+                    // fetch the search options for the query
+                    if(isset($_GET['userNameSearch'])){$optUserName = true;}else{$optUserName = false;}
+                    if(isset($_GET['firstNameSearch'])){$optFirstName = true;}else{$optFirstName = false;}
+                    if(isset($_GET['lastNameSearch'])){$optLastName = true;}else{$optLastName = false;}
+                    // Writing the correct query to fetch the correct data from the database
+                    if ($optUserName == 1) {
+                        if ($optFirstName == 1) {
+                            if ($optLastName == 1) {
+                                $query = "SELECT username, firstname, lastname FROM users WHERE username = :searchInput OR firstName = :firstInput OR lastName = :lastInput OR username LIKE :searchInput2 OR firstName LIKE :firstInput2 OR lastName LIKE :lastInput2";
+                                $stmt = $dbh->prepare($query);
+                                $stmt->execute(array(
+                                    'searchInput' => $searchInput,
+                                    'firstInput' => $searchInput,
+                                    'lastInput' => $searchInput,
+                                    'searchInput2' => '%' . $searchInput . '%',
+                                    'firstInput2' => '%' . $searchInput . '%',
+                                    'lastInput2' => '%' . $searchInput . '%'
+                                ));
                             }
                             else{
-                                if ($optLastName == 1) {
-                                    $query = "SELECT username, firstname, lastname FROM users WHERE username = :searchInput OR lastName = :lastInput";
-                                    $stmt = $dbh->prepare($query);
-                                    $stmt->execute(array(
-                                        'searchInput' => $searchInput,
-                                        'lastInput' => $searchInput
-                                    ));
-                                }
-                                else{
-                                    $query = "SELECT username, firstname, lastname FROM users WHERE username = :searchInput";
-                                    $stmt = $dbh->prepare($query);
-                                    $stmt->execute(array(
-                                        'searchInput' => $searchInput
-                                    ));
-                                }
+                                $query = "SELECT username, firstname, lastname FROM users WHERE username = :searchInput OR firstName = :firstInput OR username LIKE :searchInput2 OR firstName LIKE :firstInput2";
+                                $stmt = $dbh->prepare($query);
+                                $stmt->execute(array(
+                                    'searchInput' => $searchInput,
+                                    'firstInput' => $searchInput,
+                                    'searchInput2' => '%' . $searchInput . '%',
+                                    'firstInput2' => '%' . $searchInput . '%'
+                                ));
                             }
                         }
-                        else {
-                            if ($optFirstName == 1) {
-                                if ($optLastName == 1) {
-                                    $query = "SELECT username, firstname, lastname FROM users WHERE firstName = :firstInput OR lastName = :lastInput";
-                                    $stmt = $dbh->prepare($query);
-                                    $stmt->execute(array(
-                                        'firstInput' => $searchInput,
-                                        'lastInput' => $searchInput
-                                    ));
-                                }
-                                else{
-                                    $query = "SELECT username, firstname, lastname FROM users WHERE firstName = :firstInput";
-                                    $stmt = $dbh->prepare($query);
-                                    $stmt->execute(array(
-                                        'firstInput' => $searchInput
-                                    ));
-                                }
+                        else{
+                            if ($optLastName == 1) {
+                                $query = "SELECT username, firstname, lastname FROM users WHERE username = :searchInput OR lastName = :lastInput OR username LIKE :searchInput2 OR lastName LIKE :lastInput2";
+                                $stmt = $dbh->prepare($query);
+                                $stmt->execute(array(
+                                    'searchInput' => $searchInput,
+                                    'lastInput' => $searchInput,
+                                    'searchInput2' => '%' . $searchInput . '%',
+                                    'lastInput2' => '%' . $searchInput . '%'
+                                ));
                             }
                             else{
-                                if ($optLastName == 1) {
-                                    $query = "SELECT username, firstname, lastname FROM users WHERE lastName = :lastInput";
-                                    $stmt = $dbh->prepare($query);
-                                    $stmt->execute(array(
-                                        'lastInput' => $searchInput
-                                    ));
-                                }
-                                else {
-                                    echo '<p class=error>Please check at least one search category.</p>';
-                                }
+                                $query = "SELECT username, firstname, lastname FROM users WHERE username = :searchInput OR username LIKE :searchInput2";
+                                $stmt = $dbh->prepare($query);
+                                $stmt->execute(array(
+                                    'searchInput' => $searchInput,
+                                    'searchInput2' => '%' . $searchInput . '%'
+                                ));
                             }
                         }
-                        $results = $stmt->fetchAll();
-                        if (empty($results)) {
-                            $num = 0;
-                            //$reSpaced = reWorkSpace($searchInput);
+                    }
+                    else {
+                        if ($optFirstName == 1) {
+                            if ($optLastName == 1) {
+                                $query = "SELECT username, firstname, lastname FROM users WHERE firstName = :firstInput OR lastName = :lastInput OR firstName LIKE :firstInput2 OR lastName LIKE :lastInput2";
+                                $stmt = $dbh->prepare($query);
+                                $stmt->execute(array(
+                                    'firstInput' => $searchInput,
+                                    'lastInput' => $searchInput,
+                                    'firstInput2' => '%' . $searchInput . '%',
+                                    'lastInput2' => '%' . $searchInput . '%'
+                                ));
+                            }
+                            else{
+                                $query = "SELECT username, firstname, lastname FROM users WHERE firstName = :firstInput OR  firstName LIKE :firstInput2";
+                                $stmt = $dbh->prepare($query);
+                                $stmt->execute(array(
+                                    'firstInput' => $searchInput,
+                                    'firstInput2' => '%' . $searchInput . '%'
+                                ));
+                            }
                         }
-                        if (empty($results)) {
-                            echo '<p>Sorry, no results were found for that search.</p>';
+                        else{
+                            if ($optLastName == 1) {
+                                $query = "SELECT username, firstname, lastname FROM users WHERE lastName = :lastInput OR lastName LIKE :lastInput2";
+                                $stmt = $dbh->prepare($query);
+                                $stmt->execute(array(
+                                    'lastInput' => $searchInput,
+                                    'lastInput2' => '%' . $searchInput . '%'
+                                ));
+                            }
+                            else {
+                                echo '<p class=error>Please check at least one search category.</p>';
+                            }
                         }
-                        else {
+                    }
+                    $results = $stmt->fetchAll();
+                    if (empty($results)) {
+                        echo '<p>Sorry, no results were found for that search.</p>';
+                    }
+                    else {
             ?>
             <table>
                 <?php
-
-                            foreach ($results as $row) {
-                                echo '<tr>';
-                                if (!empty($row[0])) {
-                                    echo '<td>' . 'Username: ' . $row[0] . '</td>';
-                                }
-                                if (!empty($row[1])) {
-                                    echo '<td>' . 'First Name: ' . $row[1] . '</td>';
-                                }
-                                if (!empty($row[2])) {
-                                    echo '<td>' . 'Last Name: ' . $row[2] . '</td>';
-                                }
-                                echo '</tr>';
+                        foreach ($results as $row) {
+                            echo '<tr>';
+                            if (!empty($row[0])) {
+                                echo '<td>' . 'Username: ' . $row[0] . '</td>';
                             }
+                            if (!empty($row[1])) {
+                                echo '<td>' . 'First Name: ' . $row[1] . '</td>';
+                            }
+                            if (!empty($row[2])) {
+                                echo '<td>' . 'Last Name: ' . $row[2] . '</td>';
+                            }
+                            echo '</tr>';
                         }
-
-
+                    }
                 ?>
             </table>
             <?php
-                    }
-                }
-                else {
-                    echo '<p class="error">Please insert some text into the search bar.</p>';
                 }
             }
-
-            // Writing the functions to try if there are no search results in order to 'fix' input errors
-
-            //  echo '<p>' . . '</p>';
-            //The function to take out spaces and do magical things with results (hopefully)
-       /*     function reWorkSpace($input) {
-                //Testing to see if the input has any spaces inside of it
-                $test = stristr($input,' ');
-                if (!($test == false)) {
-                    $reInsert = substr($test, 1);
-                    echo '<p>' . $test . '</p>';
-                    $store[0] = stristr($input, ' ', -1);
-                    $test = stristr($reInsert, ' ');
-
-                    $tracker = 1;
-                    if (!($test == false)) {
-                        while (!($test == false)) {
-                            $store[$tracker] = stristr($input, ' ', -1);
-                            $tracker++;
-                            $test = substr($test, 1);
-                        }
-                    }
-                }
-
-
-               /* $test = stristr($input,' ');
-                $tracker = 0;
-                if ($test) {
-                $output[$tracker] = stristr($input);
-                    while (true) {
-                        if ($test) {
-                            $tracker ++;
-                            $output[$tracker] = ;
-                            $reInsert = substr($test, 1);
-
-                            $test = stristr($reInsert,'');
-                        }
-                        else{
-                            return $output;
-                            break;
-                        }
-                    }
-                }
-                else{
-                    return null;
-                }
+            else {
+                echo '<p class="error">Please insert some text into the search bar.</p>';
             }
-
-            /*
-            while (true) {
-                if ('test') { // is initial condition true
-                    // do something that also changes initial condition
-                }
-                else { // condition failed
-                    break; // leave loop
-                }
-            }
-            */
             ?>
         </div>
     </div>
