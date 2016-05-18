3f5aa8dd977680b112320dea8e4463d115ff7d84
Azaniyah Shephard <session1@START-Coding-MBP-22.local>
2016-05-18 13:41:56 -0700

    Added the background to the sign up page

--- signup.php
+++ signup.php
@@ -24,7 +24,9 @@ $page_title = 'Sign Up';
     </div>
 </div>
 <div class="container">
-
+    <video autoplay loop muted poster="empty.png" id="bgvid">
+        <source src="login.mp4" type="video/mp4">
+    </video>
     <div class="content">
         <?php
 
